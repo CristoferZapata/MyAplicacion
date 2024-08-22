21ac6df4b960a009643e8b1192fd5d91f717af66
Criss <133412525+CristoferZapata@users.noreply.github.com>
2024-08-22 13:51:25 -0400

    a

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private router: Router) {}
 
-  constructor() {}
+  user = {
+    username: '',
+    password: '',
+  };
+  mensaje = '';
 
+  validar() {
+    if (this.user.username.length != 0) {
+      if (this.user.password.length != 0) {
+        //Funciona
+        this.mensaje = 'Conexion exitosa';
+        let navigationExtras: NavigationExtras = {
+          state: {
+            username: this.user.username,
+            password: this.user.password,
+          },
+        };
+        this.router.navigate(['/perfil'], navigationExtras);
+      } else {
+        console.log('Contraseña vacia');
+        this.mensaje = 'Contraseña vacia';
+        //No funciona
+      }
+    } else {
+      console.log('Usuario vacio');
+      this.mensaje = 'Usuario Vacio';
+      //Tampoco funciona
+    }
+  }
 }
